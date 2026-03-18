55fcefe838634be613701f4d4d7ab439267cc23a
mayanknagle18 <mayank.nagle19@gmail.com>
2026-03-18 18:29:41 +0530

    added text

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -21,7 +21,7 @@
         <div class="lookalike-body-center">
           <div class="lookalike-inner-body">
             <h3>You don't have any leads</h3>
-            <p>Create a lead list to start running campaigns</p>
+            <p>Create a lead list to start running campaigns.</p>
             <div class="lookalike-btn-wrap">
               <v-btn color="primary" @click="createList">Create a List</v-btn>
             </div>
