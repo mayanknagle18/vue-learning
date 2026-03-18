24ed897ef226dacb2c9a51269bd82b9fabdfe4c7
mayanknagle-sys <mayanknagle@bitcot.com>
2026-03-18 18:33:11 +0530

    new change in css

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -20,7 +20,7 @@
       <div class="lookalike-body">
         <div class="lookalike-body-center">
           <div class="lookalike-inner-body">
-            <h3>You don't have any leads</h3>
+            <h3>You don't have any leads.</h3>
             <p>Create a lead list to start running campaigns.</p>
             <div class="lookalike-btn-wrap">
               <v-btn color="primary" @click="createList">Create a List</v-btn>
